856c8c6d1eb9b900d513459e7ecf13d1ed339b83
Daniel Pantea <dani.pantea@gmail.com>
2022-09-15 16:47:38 +0300

    upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// initial numarul este ascuns cu stelute
+
+// buton de afisare date
+
+// cere parola
+
+// daca parola e ok se afiseaza numarul cardului
+
+// timer 30 de secunde afisare
+
+// plus buton de ascundere manuala
+
+
