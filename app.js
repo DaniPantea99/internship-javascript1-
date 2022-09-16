ddbd212fa666bc435f30f2ab16adec434fab82f9
Daniel Pantea <dani.pantea@gmail.com>
2022-09-16 16:43:20 +0300

    update 16sep

--- app.js
+++ app.js
@@ -11,3 +11,28 @@
 // plus buton de ascundere manuala
 
 
+const btnShowDetails = document.querySelector('.btn__showDetails');
+const hide = document.querySelector('.hide');
+
+
+
+btnShowDetails.addEventListener('click', showDetails);
+
+// console.log(hide.innerText);
+
+
+
+function showDetails(hide) {
+    // let hideNr = hide.innerText;
+    let hideNr = [];
+    // console.log(hideNr);
+    // console.log(hideNr.length);
+    for(let i = 0; i < hide.length; i++) {
+        if (i < hide.length-1){
+            hideNr.push('*')
+        } else {
+            hideNr.push(hide[i])
+        }
+    }
+    return hideNr.join("");
+}
