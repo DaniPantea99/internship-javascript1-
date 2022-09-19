ef38ca6c29729f90d25aa6638373b6f506d73730
Daniel Pantea <dani.pantea@gmail.com>
2022-09-19 16:27:39 +0300

    update 19sep

--- app.js
+++ app.js
@@ -10,29 +10,78 @@
 
 // plus buton de ascundere manuala
 
-
 const btnShowDetails = document.querySelector('.btn__showDetails');
-const hide = document.querySelector('.hide');
+const showNumber = document.querySelector('.privacy-number');
+const stopwatch = document.getElementById('stopwatch');
+btnShowDetails.addEventListener('click', show);
+const countdownEl = document.getElementById('countdown');
+let timerInstance = null;
 
 
 
-btnShowDetails.addEventListener('click', showDetails);
-
-// console.log(hide.innerText);
-
-
-
-function showDetails(hide) {
-    // let hideNr = hide.innerText;
-    let hideNr = [];
-    // console.log(hideNr);
-    // console.log(hideNr.length);
-    for(let i = 0; i < hide.length; i++) {
-        if (i < hide.length-1){
-            hideNr.push('*')
+function show() {
+    clearInterval(timerInstance);
+    showNumber.classList.toggle('hide-nr');
+    if (showNumber.classList.contains('hide-nr')) {
+        // btnShowDetails.innerText = 'Show Details';
+        // stopwatch.style.visibility = 'hidden';
+        hideDetails();
+    } else {
+        const isCorrect = askForPassword();
+        if(isCorrect){
+            btnShowDetails.innerText = 'Hide Details';
+            stopwatch.style.visibility = 'visible';
+            startTimer(10);
         } else {
-            hideNr.push(hide[i])
+            showNumber.classList.toggle('hide-nr');
         }
+        
     }
-    return hideNr.join("");
 }
+
+function hideDetails() {
+    btnShowDetails.innerText = 'Show Details';
+    stopwatch.style.visibility = 'hidden';
+}
+
+function startTimer(seconds) {
+    // reset timer 
+    countdownEl.innerText = `0 : ${seconds}`;
+    timerInstance = setInterval(function () {
+        seconds--;
+        if(seconds >= 0) {
+            countdownEl.innerText = '0 : '+ seconds;
+        }  else {
+            clearInterval(timerInstance);
+            // btnShowDetails.innerText = 'Show Details';
+            // stopwatch.style.visibility = 'hidden';
+            hideDetails();
+            showNumber.classList.toggle('hide-nr');
+        }
+    }, 1000);  
+}
+
+
+function askForPassword() {
+    const password = "1";
+    let input;
+    let count = 0;
+    let limit = 3;
+    let error = false;
+
+    while (input != password && !error) {
+        if(count < limit) {
+            input = window.prompt("Enter Password");
+            count++;
+        } else {
+            error = true;
+        }
+   }
+
+   if(error){
+        alert('Ai depasit limita!');
+        return false;
+   } 
+    return true;
+}
+
